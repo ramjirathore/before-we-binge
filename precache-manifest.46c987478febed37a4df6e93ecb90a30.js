6e04b57bffa165913bd5290b73becbd95ba53e0f
Ramji Rathore <=ramjirathore2802@gmail.com>
2020-05-08 15:33:27 +0530

    Updates

---
+++ precache-manifest.46c987478febed37a4df6e93ecb90a30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f13be8fd45fc7393dd09d3dcc3093392",
+    "url": "/movies-series-app/index.html"
+  },
+  {
+    "revision": "5c25647affde1c2f37b4",
+    "url": "/movies-series-app/static/css/main.bfd314cb.chunk.css"
+  },
+  {
+    "revision": "363a61ed81dfd6f28233",
+    "url": "/movies-series-app/static/js/2.922809fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies-series-app/static/js/2.922809fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c25647affde1c2f37b4",
+    "url": "/movies-series-app/static/js/main.d5126051.chunk.js"
+  },
+  {
+    "revision": "2f772d8749e68d0d7eaf",
+    "url": "/movies-series-app/static/js/runtime-main.09d11bc7.js"
+  },
+  {
+    "revision": "8b31cde9d695d9a80760aaf61b2efd12",
+    "url": "/movies-series-app/static/media/Ripple.8b31cde9.gif"
+  }
+]);
